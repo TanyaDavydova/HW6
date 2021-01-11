3b68b4f185188c11939ee20df789558b722e721b
Tanya <75931823+TanyaDavydova@users.noreply.github.com>
2021-01-11 19:44:38 +0200

    Add files via upload

--- app6_1.js
+++ app6_1.js
@@ -1,5 +1,22 @@
-function capitalize(string) {
- return string.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()});
+function CapitaleLetter(contents){
+ var collection="";
+
+ //console.log(viu[5]==0);
+
+
+for(i=0; i<contents.length; i++){
+ if (contents[i-1]==0){
+  collection += (contents[i].toUpperCase());
+ } 
+ else {
+  collection += (contents[i].toLowerCase());
+ }
+ if(i!=contents.length-1){
+  continue;
+ }
+ console.log(collection[0].toUpperCase() + collection.slice(1));
+ }
+
 }
 
-console.log(capitalize('lorem ipsum stan'))
+CapitaleLetter("nnnnnn nnnnn nnnnn nnnnn jjy");
